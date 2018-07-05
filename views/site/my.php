b933da7a019eb73881d6e3dbd0e902856bb10ce2
Andrew66631 <andrussshka@yandex.ru>
2018-07-06 02:00:06 +0300

    changes2

--- views/site/my.php
+++ views/site/my.php
@@ -1,9 +1,10 @@
 <?php
 use yii\helpers\Html;
-use yii\helpers\Html;
 
 $this->title = 'My';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<div class="site-my">
 <h1><?= Html::encode($this->title) ?></h1>
+</div>
 <?= Html::encode($message) ?>
