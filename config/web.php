012880e5e3b7102988325b9a28b6116b0b2392d4
Andrew66631 <andrussshka@yandex.ru>
2018-07-07 18:29:57 +0300

    changes 07072018

--- config/web.php
+++ config/web.php
@@ -13,6 +13,8 @@ $config = [
     ],
     'components' => [
         'request' => [
+            'parsers'=>[
+                'application/json'=>'yii\web\JsonParser',
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '-SbYSsW6ZMSp6GkE35-pTOdROKXlucxz',
         ],
@@ -52,11 +54,7 @@ $config = [
                 ['class'=>'yii\rest\UrlRule','controller'=>'my'],
             ],
         ],
-        'request'=>[
-            'parsers'=>[
-                'application/json'=>'yii\web\JsonParser',
-            ]
-        ],
+
 
     ],
     'params' => $params,
