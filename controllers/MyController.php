e939532514c8bb8ebe5bdded1ae4db9d6402c000
Andrew66631 <andrussshka@yandex.ru>
2018-07-07 18:11:36 +0300

    changes controller

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -5,14 +5,14 @@ use yii\rest\ActiveController;
 
 class MyController extends ActiveController
 {
-	public $modelClass = 'app\models\User';
-	public function actionMy()
+	public $modelClass = 'app\models\acl_users';
+	/*public function actionMy()
     {
         $model=new acl_users();
         return $this->render('my', [
             'model' => $model,
         ]);
-    }
+    }*/
 }
 ?>
 
