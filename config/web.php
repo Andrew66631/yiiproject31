809bff659180e568fecf3139eb995f28d728d957
Andrew66631 <andrussshka@yandex.ru>
2018-07-07 18:43:30 +0300

    changes 07072018-3

--- config/web.php
+++ config/web.php
@@ -13,8 +13,6 @@ $config = [
     ],
     'components' => [
         'request' => [
-            'parsers'=>[
-                'application/json'=>'yii\web\JsonParser',]
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '-SbYSsW6ZMSp6GkE35-pTOdROKXlucxz',
         ],
@@ -46,14 +44,14 @@ $config = [
         ],
         'db' => $db,
 
-        'urlManager' => [
+        /*'urlManager' => [
             'enablePrettyUrl' => true,
             'enableStrictParsing'=>true,
             'showScriptName' => false,
             'rules' => [
                 ['class'=>'yii\rest\UrlRule','controller'=>'my'],
             ],
-        ],
+        ],*/
 
 
     ],
