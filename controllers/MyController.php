a192f54e0e7854a386e9a0cb1e74abd58e6c808a
Andrew66631 <andrussshka@yandex.ru>
2018-07-09 02:05:18 +0300

    11

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -5,23 +5,24 @@ use yii\rest\ActiveController;
 
 class MyController extends ActiveController
 {
-	//public $modelClass = 'app\models\acl_users';
-	/*public function actionMy()
+    //public $modelClass = 'app\models\acl_users';
+    /*public function actionMy()
     {
         $model=new acl_users();
         return $this->render('my', [
             'model' => $model,
         ]);
     }*/
-}
-public function actionIndex()
-{
-	$hi='Контроллер успешно отрабатывает';
-	return $this->render('index',
-	[
-		'hi'=>$hi,
-]
-	);
+
+    public function actionIndex()
+    {
+        $hi = 'Контроллер успешно отрабатывает';
+        return $this->render('index',
+            [
+                'hi' => $hi,
+            ]
+        );
+    }
 }
 ?>
 
