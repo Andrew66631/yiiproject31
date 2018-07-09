1bb5c335aed85f6a6ea9dd4caecf97340bc184b7
Andrew66631 <andrussshka@yandex.ru>
2018-07-09 23:12:38 +0300

    09072018-10

--- views/my/index.php
+++ views/my/index.php
@@ -2,7 +2,12 @@
 use yii\helpers\Html;
 use yii\widgets\LinkPager;
 /* @var $this yii\web\View */
-/* @var $hi string */
+
+$this->title = 'Site';
+$default='';
+$this->registerJsFile('@web/js/main-index.js',['position'=>$this::POS_END],'main-index');
+//$this->registerJs('alert("Привет мир")',$this::POS_READY,'hello-message');
+$this->registerCssFile('@web/css/site.css');
 ?>
 
 <p class="site-index" xmlns="http://www.w3.org/1999/html">
