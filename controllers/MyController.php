a391a41753677abecc293cdf76a178f1a1fad475
Andrew66631 <andrussshka@yandex.ru>
2018-07-11 01:19:30 +0300

    10072018 test5

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -5,5 +5,15 @@ use yii\rest\ActiveController;
 class MyController extends ActiveController
 {
     public $modelClass = 'app\models\acl_users';
+    public function actionNew()
+    {
+
+        $hi = 'Контроллер успешно отрабатывает и другие экшн';
+       return $this->render('new',
+            [
+               'hi' => $hi,
+           ]
+        );
+    }
 }
 ?>
---
+++ views/my/new.php
@@ -0,0 +1,19 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
+/* @var $this yii\web\View */
+
+$this->title = 'Site';
+$default='';
+$this->registerJsFile('@web/js/main-index.js',['position'=>$this::POS_END],'main-index');
+//$this->registerJs('alert("Привет мир")',$this::POS_READY,'hello-message');
+$this->registerCssFile('@web/css/site.css');
+?>
+
+<p class="site-index" xmlns="http://www.w3.org/1999/html">
+
+<div class="jumbotron">
+    <?= $hi ?>
+    <?= $hi ?>
+</div>
+</p>
