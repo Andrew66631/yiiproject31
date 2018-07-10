014f7536b1843f488c9d279f02f3a3ab56c4f8c8
Andrew66631 <andrussshka@yandex.ru>
2018-07-11 00:38:01 +0300

    10072018 test4

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -6,28 +6,4 @@ class MyController extends ActiveController
 {
     public $modelClass = 'app\models\acl_users';
 }
-//use yii\web\Controller;
-
-//class MyController extends Controller
-{
-    //public $modelClass = 'app\models\acl_users';
-    /*public function actionMy()
-    {
-        $model=new acl_users();
-        return $this->render('my', [
-            'model' => $model,
-        ]);
-    }*/
-    //public $modelClass = 'app\models\acl_users';
-    //public function actionIndex()
-    //{
-
-       // $hi = 'Контроллер успешно отрабатывает';
-       // return $this->render('index',
-            //[
-              //  'hi' => $hi,
-           // ]
-        //);
-   // }
-//}
 ?>
