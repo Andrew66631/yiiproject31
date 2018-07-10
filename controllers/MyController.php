05b25605bac7a4c21a472bb3ee58c7ed6eb2d99e
Andrew66631 <andrussshka@yandex.ru>
2018-07-11 02:32:57 +0300

    xml/html

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -20,7 +20,7 @@ class MyController extends ActiveController
     public function behaviors()
     {
         $behaviors = parent::behaviors();
-        $behaviors['contentNegotiator']['formats']['text/xml'] = Response::FORMAT_HTML;
+        $behaviors['contentNegotiator']['formats']['text/xml,html'] = Response::FORMAT_HTML;
         return $behaviors;
     }
     public function actionNew()
