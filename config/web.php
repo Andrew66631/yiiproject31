5e8208db51c1ebb3f7663ac7f52dbacb9aaa2abe
Andrew66631 <andrussshka@yandex.ru>
2018-07-11 01:38:58 +0300

    10072018 test6

--- config/web.php
+++ config/web.php
@@ -55,7 +55,15 @@ $config = [
             'enableStrictParsing'=>false,
             'showScriptName' => false,
             'rules' => [
-                ['class' => 'yii\rest\UrlRule', 'controller' => 'my'],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'my',
+                    'PUT,PATCH users/<id>' => 'user/update',
+                    'DELETE users/<id>' => 'user/delete',
+                    'GET,HEAD users/<id>' => 'user/view',
+                    'POST users' => 'user/create',
+                    'GET,HEAD users' => 'user/index',
+                    'users/<id>' => 'user/options',
+                    'users' => 'user/options',
+                    ],
             ],
 
         ],
