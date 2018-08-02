af828ba02f205397d3f889598490aa9eb2cdc250
Andrew66631 <andrussshka@yandex.ru>
2018-08-03 01:11:20 +0300

    3082018 commit

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -42,43 +42,5 @@ class MyController extends ActiveController
             ]
         ];
     }
-    /*public function behaviors()
-    {
-        return ArrayHelper::merge(parent::behaviors(), [
-            [
-                'class' => 'yii\filters\ContentNegotiator',
-                'only' => ['view', 'index'],  // in a controller
-                // if in a module, use the following IDs for user actions
-                // 'only' => ['user/view', 'user/index']
-                'formats' => [
-                    'application/json' => Response::FORMAT_JSON,
-                ],
-                'languages' => [
-                    'en',
-                    'de',
-                ],
-            ],
-        ]);
-    }*/
-
-    /*public function actionNew()
-    {
-
-        $query=acl_users::find();
-        $pagination= new Pagination([
-            'defaultPageSize'=>40,
-            'totalCount'=>$query->count(),
-        ]);
-        $users=$query->orderBy('id')
-            ->offset($pagination->offset)
-            ->limit($pagination->limit)
-            ->all();
-       return $this->render('new',
-            [
-                'users'=>$users,
-                'pagination'=>$pagination,
-           ]
-        );
-    }*/
 }
 ?>
