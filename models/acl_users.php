7e52533a4da5eb8e982768b8fcd3798e2d9138cc
Andrew66631 <andrussshka@yandex.ru>
2018-08-06 00:29:07 +0300

    6082018

--- models/acl_users.php
+++ models/acl_users.php
@@ -6,4 +6,15 @@ use yii\db\ActiveRecord;
 
 class acl_users extends ActiveRecord
 {
+    public static function tableName()
+    {
+        return 'articles';
+    }
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['name'], 'character varying(40)'],
+        ];
+    }
 }
